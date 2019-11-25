aaee51da4fd1f440ada47b8e9071f3bd6e41de87
easyvipin <vipinchandra555@gmail.com>
2019-11-25 07:34:12 +0530

    Updates

---
+++ precache-manifest.02602da4accbf7873591a932abb6d00e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f37be985b8bed049122934ee00e3129",
+    "url": "/SEARCH-BOX-IN-REACT/index.html"
+  },
+  {
+    "revision": "11cf5718d1d9a1d6e013",
+    "url": "/SEARCH-BOX-IN-REACT/static/css/main.9e3c172d.chunk.css"
+  },
+  {
+    "revision": "a731c8311b8e37f514cc",
+    "url": "/SEARCH-BOX-IN-REACT/static/js/2.fbe3378d.chunk.js"
+  },
+  {
+    "revision": "11cf5718d1d9a1d6e013",
+    "url": "/SEARCH-BOX-IN-REACT/static/js/main.6b9c6847.chunk.js"
+  },
+  {
+    "revision": "197525fb649dc1e3aa86",
+    "url": "/SEARCH-BOX-IN-REACT/static/js/runtime-main.2c1c1fd4.js"
+  },
+  {
+    "revision": "e2af9e7144acf955f895c56ba7f611e7",
+    "url": "/SEARCH-BOX-IN-REACT/static/media/back2.e2af9e71.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/SEARCH-BOX-IN-REACT/static/media/logo.25bf045c.svg"
+  }
+]);
